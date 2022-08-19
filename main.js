22475f60039edb6fb8814024bc1ef37c7076418f
Irina-Dehtiarenko <93826301+Irina-Dehtiarenko@users.noreply.github.com>
2022-08-19 23:09:11 +0200

    created the basic HTML structure and CSS style, and I wrote the notes to be taken

---
+++ main.js
@@ -0,0 +1,20 @@
+// moje zadania
+
+// Wypełnij formularz i zobacz aktualizację danych karty w czasie rzeczywistym
+
+// Otrzymuj komunikaty o błędach po przesłaniu formularza, jeśli:
+// Każde pole wejściowe jest puste Numer karty,
+// data ważności lub pola CVC mają nieprawidłowy format
+
+// Zobacz optymalny układ w zależności od rozmiaru ekranu urządzenia
+
+// Zobacz stan najechania, aktywności i fokusu dla interaktywnych elementów na stronie
+
+// _____________________________________________
+
+//polecenia:
+// 1. Aktualizuj dane na karcie, gdy użytkownik wypełnia pola
+// 2. Sprawdź poprawność pól formularza po prz.esłaniu formularza
+// 3. Jeśli nie ma błędów, wyświetl stan ukończenia
+// 4. Zresetuj formularz, gdy użytkownik kliknie „Kontynuuj” w wypełnionym stanie
+//  WAŻNE ⚠️: Kiedy tworzysz wersję swojego projektu na żywo, zalecamy nadanie jej losowej nazwy dla adresu URL. Dzieje się tak, ponieważ ponieważ jest to fałszywy formularz karty kredytowej, niektóre przeglądarki otworzą ostrzeżenie, zanim ludzie będą mogli uzyskać dostęp do Twojego projektu. Dlatego najlepiej nadać projektowi nazwę niezwiązaną z kartami kredytowymi, aby przeglądarki nie wyświetlały ostrzeżenia osobom przeglądającym Twój projekt.
