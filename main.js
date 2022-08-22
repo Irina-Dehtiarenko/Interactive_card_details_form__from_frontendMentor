d37fd14e3c1b6671b614612716ed94277f64ce7f
Irina-Dehtiarenko <93826301+Irina-Dehtiarenko@users.noreply.github.com>
2022-08-22 19:46:23 +0200

    created a function for adding cardholder name and card number in rel time by typing into input

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Najpierw zrobić STYLE for MOBILE
+
 // moje zadania
 
 // Wypełnij formularz i zobacz aktualizację danych karty w czasie rzeczywistym
@@ -18,3 +20,33 @@
 // 3. Jeśli nie ma błędów, wyświetl stan ukończenia
 // 4. Zresetuj formularz, gdy użytkownik kliknie „Kontynuuj” w wypełnionym stanie
 //  WAŻNE ⚠️: Kiedy tworzysz wersję swojego projektu na żywo, zalecamy nadanie jej losowej nazwy dla adresu URL. Dzieje się tak, ponieważ ponieważ jest to fałszywy formularz karty kredytowej, niektóre przeglądarki otworzą ostrzeżenie, zanim ludzie będą mogli uzyskać dostęp do Twojego projektu. Dlatego najlepiej nadać projektowi nazwę niezwiązaną z kartami kredytowymi, aby przeglądarki nie wyświetlały ostrzeżenia osobom przeglądającym Twój projekt.
+
+const inputName = document.querySelector(".input-name");
+const cardholderName = document.querySelector("p.cardholder-name");
+const inputNumber = document.querySelector(".input-card-number");
+const cardNumber = document.querySelector("p.card-number");
+
+//Imię
+const changeCardholderName = (e) => {
+  cardholderName.textContent = inputName.value;
+};
+
+inputName.addEventListener("keyup", changeCardholderName);
+
+//numer karty
+const changeCardNumber = (e) => {
+  if (inputNumber.value.length < 20) {
+    cardNumber.textContent = inputNumber.value;
+
+    console.log(inputNumber.value.length);
+  } else {
+    alert(
+      "the maximum length of the card number - 19 characters (including 3 spaces), please enter it correctly, according to the example "
+    );
+    cardNumber.textContent = "0000 0000 0000 0000";
+    return (inputNumber.value = "");
+    // inputNumber.removeEventListener("keyup", changeCardNumber);
+  }
+};
+
+inputNumber.addEventListener("keyup", changeCardNumber);
