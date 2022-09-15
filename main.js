243d0733c50fefe2cc8d993781c13e8cf5d4f807
Irina-Dehtiarenko <93826301+Irina-Dehtiarenko@users.noreply.github.com>
2022-09-15 17:31:26 +0200

    added the function of changing the active form on a complete form, if the form is correct. And I have some problems with the name inputs style

--- main.js
+++ main.js
@@ -1,85 +1,55 @@
-// Najpierw zrobić STYLE for MOBILE
-
-// moje zadania
-
-// Wypełnij formularz i zobacz aktualizację danych karty w czasie rzeczywistym
-
-// Otrzymuj komunikaty o błędach po przesłaniu formularza, jeśli:
-// Każde pole wejściowe jest puste Numer karty,
-// data ważności lub pola CVC mają nieprawidłowy format
-
-// Zobacz optymalny układ w zależności od rozmiaru ekranu urządzenia
-
-// Zobacz stan najechania, aktywności i fokusu dla interaktywnych elementów na stronie
-
-// _____________________________________________
-
-//polecenia:
-// 1. Aktualizuj dane na karcie, gdy użytkownik wypełnia pola
-// 2. Sprawdź poprawność pól formularza po prz.esłaniu formularza
-// 3. Jeśli nie ma błędów, wyświetl stan ukończenia
-// 4. Zresetuj formularz, gdy użytkownik kliknie „Kontynuuj” w wypełnionym stanie
-//  WAŻNE ⚠️: Kiedy tworzysz wersję swojego projektu na żywo, zalecamy nadanie jej losowej nazwy dla adresu URL. Dzieje się tak, ponieważ ponieważ jest to fałszywy formularz karty kredytowej, niektóre przeglądarki otworzą ostrzeżenie, zanim ludzie będą mogli uzyskać dostęp do Twojego projektu. Dlatego najlepiej nadać projektowi nazwę niezwiązaną z kartami kredytowymi, aby przeglądarki nie wyświetlały ostrzeżenia osobom przeglądającym Twój projekt.
-
 const inputName = document.querySelector(".input-name");
 const cardholderName = document.querySelector("p.cardholder-name");
+
 const inputNumber = document.querySelector(".input-card-number");
 const cardNumber = document.querySelector("p.card-number");
+
 const inputMonth = document.querySelector(".input-month");
 const spanMonth = document.querySelector(".month");
+
 const inputYear = document.querySelector(".input-year");
 const spanYear = document.querySelector(".year");
+
 const inputCvc = document.querySelector(".input-cvc");
 const pCvc = document.querySelector("p.cvc");
+
 const buttonActiveForm = document.querySelector(".active-form form button");
 const buttonCompleteForm = document.querySelector(".complete-form button");
 const articleActiveForm = document.querySelector(".active-form");
 const articleCompleteForm = document.querySelector(".complete-form ");
 const form = document.querySelector("form");
-console.log(form);
 const inputs = [...document.querySelectorAll("input")];
-// const pError = [...document.querySelectorAll("p.error")];
-
-// const pErrorName = document.querySelector(".error__name");
-// const pErrorNumber = document.querySelector(".error__number");
-// const pErrorMonth = document.querySelector(".error__month");
-// const pErrorYear = document.querySelector(".error__year");
-// const pErrorCvc = document.querySelector(".error__cvc");
-
 const divsFormControl = [...document.querySelectorAll(".form-control")];
-console.log(inputs);
 
 // alert(
 //   "This is an example page for science. Please do not enter correct data in the form fields!"
 // );
 
-// //Imię
+inputName.style.borderColor = "hsl(279, 6%, 84%)";
+
+// Cardholder name
 const changeCardholderName = (e) => {
   cardholderName.textContent = inputName.value;
 };
 
-// //numer karty
+// card number
 const changeCardNumber = (e) => {
   cardNumber.textContent = inputNumber.value;
-
-  // cardNumber.textContent = "0000 0000 0000 0000";
-
-  // inputNumber.removeEventListener("keyup", changeCardNumber);
 };
+//expiration date
 const changeMonth = (e) => {
   spanMonth.textContent = inputMonth.value;
 };
 const changeYear = () => {
   spanYear.textContent = inputYear.value;
 };
+
+//CVC code
 const changeCvc = () => {
   pCvc.textContent = inputCvc.value;
 };
-const completeForm = (e) => {
-  articleActiveForm.classList.toggle("not-active");
-  articleCompleteForm.classList.toggle("not-active");
-};
 
+// the error function
 const setErrorFor = (input, message) => {
   const formControl = input.parentElement;
   const pError = formControl.querySelector("p.error");
@@ -88,6 +58,7 @@ const setErrorFor = (input, message) => {
   pError.textContent = message;
 };
 
+//the success function
 const setSuccessFor = (input) => {
   const formControl = input.parentElement;
   const pError = formControl.querySelector("p.error");
@@ -96,6 +67,7 @@ const setSuccessFor = (input) => {
   pError.textContent = "";
 };
 
+// Data checking
 const dataChecking = () => {
   const cardholderNameValue = inputName.value.trim();
   const cardNumberValue = inputNumber.value.trim();
@@ -103,79 +75,86 @@ const dataChecking = () => {
   const yearValue = inputYear.value.trim();
   const cvcValue = inputCvc.value.trim();
 
-  //check name input
+  if (
+    divsFormControl[0].className.includes("form-control error") ||
+    divsFormControl[1].className.includes("form-control error") ||
+    divsFormControl[2].className.includes("form-control error") ||
+    divsFormControl[3].className.includes("form-control error")
+    // || divsFormControl[4].className.includes("form-control error")
+  ) {
+    //checking the name input
 
-  divsFormControl.forEach((div) => {
-    if (div.className === "form-control error") {
-      if (cardholderNameValue === "") {
-        setErrorFor(inputName, "Can't be blank");
-      } else if (
-        cardholderNameValue.includes("0") ||
-        cardholderNameValue.includes("1") ||
-        cardholderNameValue.includes("2") ||
-        cardholderNameValue.includes("3") ||
-        cardholderNameValue.includes("4") ||
-        cardholderNameValue.includes("5") ||
-        cardholderNameValue.includes("6") ||
-        cardholderNameValue.includes("7") ||
-        cardholderNameValue.includes("8") ||
-        cardholderNameValue.includes("9") ||
-        cardholderNameValue.includes(" ")
-      ) {
-        setErrorFor(inputName, "Wrong format, letters only");
-      } else {
-        setSuccessFor(inputName);
-      }
-
-      //check card number input
-
-      if (cardNumberValue === "") {
-        setErrorFor(inputNumber, "Can't be blank");
-      } else {
-        setSuccessFor(inputNumber);
-      }
-
-      //Check Month input
-
-      if (monthValue === "") {
-        setErrorFor(inputMonth, "Can't be blank");
-      } else {
-        if (monthValue * 1 > 12) {
-          setErrorFor(inputMonth, "There are olny 12 month in year");
-        } else {
-          setSuccessFor(inputMonth);
-        }
-      }
-
-      //Check Year input
-
-      if (yearValue === "") {
-        setErrorFor(inputYear, "Can't be blank");
-        // inputYear.parentElement.classList.add("error");
-      } else {
-        if (yearValue * 1 < 22 || yearValue * 1 > 50) {
-          setErrorFor(inputYear, "Your card is not valid");
-        } else {
-          setSuccessFor(inputYear);
-        }
-      }
-
-      //Check CVC input
-
-      if (cvcValue === "") {
-        setErrorFor(inputCvc, "Can't be blank");
-      } else {
-        setSuccessFor(inputCvc);
-      }
-    } else if (div.className === "form-control") {
-      // zły warunek
-      console.log("niema klasy error");
-      // articleActiveForm.classList.add("not-active");
-      // articleCompleteForm.classList.remove("not-active");
+    if (cardholderNameValue === "") {
+      inputName.style.borderColor = "red";
+      setErrorFor(inputName, "Can't be blank");
+    } else if (
+      cardholderNameValue.includes("0") ||
+      cardholderNameValue.includes("1") ||
+      cardholderNameValue.includes("2") ||
+      cardholderNameValue.includes("3") ||
+      cardholderNameValue.includes("4") ||
+      cardholderNameValue.includes("5") ||
+      cardholderNameValue.includes("6") ||
+      cardholderNameValue.includes("7") ||
+      cardholderNameValue.includes("8") ||
+      cardholderNameValue.includes("9") ||
+      cardholderNameValue.includes(" ")
+    ) {
+      setErrorFor(inputName, "Wrong format, letters only");
+      inputName.style.borderColor = "red";
+    } else {
+      inputName.style.borderColor = "hsl(279, 6%, 84%)";
+      setSuccessFor(inputName);
     }
-  });
+
+    //checking the card number input
+
+    if (cardNumberValue === "") {
+      setErrorFor(inputNumber, "Can't be blank");
+    } else {
+      setSuccessFor(inputNumber);
+    }
+
+    //Checking the Month input
+
+    if (monthValue === "") {
+      setErrorFor(inputMonth, "Can't be blank");
+    } else {
+      if (monthValue * 1 > 12) {
+        setErrorFor(inputMonth, "There are olny 12 month in year");
+      } else {
+        setSuccessFor(inputMonth);
+      }
+    }
+
+    //Checking the Year input
+
+    if (yearValue === "") {
+      setErrorFor(inputYear, "Can't be blank");
+      // inputYear.parentElement.classList.add("error");
+    } else {
+      if (yearValue * 1 < 22 || yearValue * 1 > 50) {
+        setErrorFor(inputYear, "Your card is not valid");
+      } else {
+        setSuccessFor(inputYear);
+      }
+    }
+
+    //Checking the CVC input
+
+    if (cvcValue === "") {
+      setErrorFor(inputCvc, "Can't be blank");
+    } else {
+      setSuccessFor(inputCvc);
+    }
+  } else {
+    //if there is no ' error' class on inputs
+    articleActiveForm.classList.add("not-active");
+    articleCompleteForm.classList.remove("not-active");
+  }
 };
 
+// the add data function
 const addData = (e) => {
   if (e.target.className === "input-name") {
     changeCardholderName();
@@ -194,34 +173,11 @@ inputs.forEach((input) => {
   input.addEventListener("keyup", addData);
 });
 
-// buttonActiveForm.addEventListener("click", (e) => {
-//   dataChecking();
-//   // e.preventDefault();
-// });
-
-/* 
-const completeForm = (e) => {
-  articleActiveForm.classList.toggle("not-active");
-  articleCompleteForm.classList.toggle("not-active");
-};
-*/
-
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   dataChecking();
-  // divsFormControl.forEach((div) => {
-  //   if (div.className === "form-control error") {
-  //     console.log("Tak");
-  //     dataChecking();
-  //   } else {
-  //     console.log("Nie");
-  //     articleActiveForm.classList.toggle("not-active");
-  //     articleCompleteForm.classList.toggle("not-active");
-  //   }
-  // });
 });
 
-buttonCompleteForm.addEventListener("click", (e) => {
-  e.preventDefault();
-  completeForm();
+buttonCompleteForm.addEventListener("click", () => {
+  window.location.reload(true);
 });
